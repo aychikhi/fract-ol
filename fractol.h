4737415dbf8c4f1da73d5eeec2a3ea8b20f22bc5
Aymane Chikhi <aychikhi@c2r11p4.1337.ma>
2025-01-09 11:06:30 +0100

    hh

--- fractol.h
+++ fractol.h
@@ -1,10 +1,18 @@
 #ifndef FRACOL_H
 # define FRACOL_H
 
+typedef struct s_complex
+{
+	//x
+	double real;
+	//imaginaire
+	double i;
+}t_complex;
+
 #include <stdio.h>
 #include <unistd.h>
 #include <math.h>
 #include <stdlib.h>
-#include <mlx.h>
+#include "MLX/include/MLX42/MLX42.h"
 
 # endif
---
+++ main.c
@@ -0,0 +1,28 @@
+#include "fractol.h"
+
+// t_complex	plane_cord(t_complex c)
+// {
+// 	t_complex z;
+// 	z.real = 0;
+// 	z.i = 0;
+// 	double tmp_z;
+// 	tmp_z = (z.real * z.real) - (z.i * z.i);
+// 	z.i += 2 * z.i * z.real;
+// 	z.real = tmp_z;
+// 	z.real += c.real;
+// 	z.i += c.i;
+// 	return z;
+// }
+
+// int main()
+// {
+// 	mlx_t *win = mlx_init(500, 500, "Mandelbrot", false);
+// 	mlx_image_t *img = mlx_new_image(win, 500, 500);
+// 	mlx_image_to_window(win, img, 0, 0);
+// 	mlx_loop(win);
+// }
+
+int main(int ac, char **av)
+{
+	
+}
